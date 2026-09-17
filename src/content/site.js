594d821c819e7e8a63cb05e9b625c3c92f6fdbb2
Yamen Bashaireh <bashyamen1@gmail.com>
2026-09-17 05:33:29 +0300

    update email

--- src/content/site.js
+++ src/content/site.js
@@ -3,7 +3,7 @@ export const site = {
   url: 'https://www.smasurfacesolutions.com',
   phone: '+1 (647) 712-0706',
   phoneHref: 'tel:+16477120706',
-  email: 'smasolutaions@gmail.com',
+  email: 'smasolutions97@gmail.com',
   whatsapp: 'https://wa.me/16477120706',
   maps: 'https://maps.app.goo.gl/1cvX34grGMmmZcRs6',
   instagram: 'https://www.instagram.com/sma_surface_solution/',
